559528a4fc14bc688251103396bb63281575460a
Anindya Pramudita <anindyapramudita2111@gmail.com>
2024-04-12 13:29:12 +0200

    feat(component): expose new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
+export { ArcImage, ArcSlideImages } from "./Image";
+export { Button } from "./Button";
+export { Hero } from "./Hero";
+export { PageLayout } from "./Layout";
 export { Navbar } from "./Navbar";
