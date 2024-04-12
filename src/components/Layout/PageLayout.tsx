dde63ac039ffdff69a6b14d06b40c01765e2fbf5
Anindya Pramudita <anindyapramudita2111@gmail.com>
2024-04-12 13:29:02 +0200

    feat(page-layout): expose page layout

---
+++ src/components/Layout/PageLayout.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import { Hero, Navbar } from "..";
+
+interface IPageLayoutProps {
+  content: any;
+  slug?: string;
+}
+
+export const PageLayout: FC<IPageLayoutProps> = ({ content, slug }) => {
+  const { navbar, hero } = content;
+  return (
+    <div className="min-h-screen w-screen">
+      <div className="w-screen fixed">
+        {navbar && <Navbar navbar={navbar} slug={slug} />}
+      </div>
+      <div className="pt-[68px] lg:pt-20">{hero && <Hero {...hero} />}</div>
+    </div>
+  );
+};
