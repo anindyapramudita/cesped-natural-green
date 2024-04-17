2ba778b4907658ac13d5327564c932da929a1050
Anindya Pramudita <anindyapramudita2111@gmail.com>
2024-04-17 09:11:44 +0200

    feat(page-layout): implement section to page layout

--- src/components/Layout/PageLayout.tsx
+++ src/components/Layout/PageLayout.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 import { Hero, Navbar } from "..";
+import { SectionLayout } from ".";
+import { TSection } from "@/shared/types/data.types";
 
 interface IPageLayoutProps {
   content: any;
@@ -7,13 +9,20 @@ interface IPageLayoutProps {
 }
 
 export const PageLayout: FC<IPageLayoutProps> = ({ content, slug }) => {
-  const { navbar, hero } = content;
+  const { navbar, hero, sections } = content;
   return (
     <div className="min-h-screen w-screen">
       <div className="z-10 w-screen fixed">
         {navbar && <Navbar navbar={navbar} slug={slug} />}
       </div>
-      <div className="pt-[68px] lg:pt-20">{hero && <Hero {...hero} />}</div>
+      <div className="pt-[68px] lg:pt-20">
+        {hero && <Hero {...hero} />}
+        {sections &&
+          sections.length > 0 &&
+          sections.map((section: TSection) => (
+            <SectionLayout {...section} key={section._key} />
+          ))}
+      </div>
     </div>
   );
 };
